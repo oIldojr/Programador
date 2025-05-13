8c0a6599223a90894e2abd67ae5a340b3313dfa8
Ildo <ildomoura90@gmail.com>
2025-05-13 10:43:47 -0300

    Atualizando arquivo

--- estudantes.py
+++ estudantes.py
@@ -1,7 +1,7 @@
 estudantes = ["Maria","João","Ana","Pedro"]
 estudantes.append("Carlos") # Adicionado o Cralos
 estudantes.sort() # Organizando em ordem alfábetica
-estudantes.remove("Ana") # Removendo a Ana
+estudantes.remove("Pedro") # Removendo o Pedro
 estudantes.pop() # Removendo o Pedro
 qtd=len(estudantes)
 print(qtd) # Estado da lista final
