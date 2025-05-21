e500902107dfda6701bac9938a015352441a9ba2
Ildo <ildomoura90@gmail.com>
2025-05-21 11:12:49 -0300

    Projeto quase finalizado

--- Projeto2.0.py
+++ Projeto2.0.py
@@ -1,17 +1,30 @@
 from abc import ABC, abstractmethod
 
 class Tarefa():                                      #Criação das Classes 
-    def __init__(self,descriçao,prioridade,titulo):
-        self.descrião = descriçao
-        self.status = status
+    def __init__(self,descricao,prioridade,titulo):
+        self.descriao = descricao
         self.prioridade = prioridade                    #Iniciação dos parâmetros
-        self.titulo = titulo                                        
+        self.titulo = titulo     
+
+    def  __str__(self):
+        return f"Tarefa: {self.titulo} - {self.descricao} - {self.prioridade}"                                   
                                  
 class Usuario():
-    def __init__(nome,mail,senha):
+    def __init__(self, nome,mail,senha):
         self.nome = nome
         self.mail = mail 
         self.senha = senha
+        self.lista_tarefas = []
+
+
+    def criar_tarefa(self,descricao,prioridade,titulo):       # Método para criar tarefas
+        nova_tarefa = Tarefa(titulo,descricao,prioridade)
+        self.lista_tarefas.append(nova_tarefa)
+
+
+    def remover_tarefa(self):                   # Método para remover tarefas
+        self.lista_tarefas.remove()
+
 
 
 class Funcionalidades:
@@ -20,9 +33,9 @@ class Funcionalidades:
         self.usuario_logado = None
 
     def cadastrar_usuario(self):  #Cadastrando novo usuario
-        nome = input("Digite um nome")
-        mail = input("Digite o E-mail")
-        senha = input("Crie uma senha")
+        nome = input("Digite um nome: ")
+        mail = input("Digite o E-mail: ")
+        senha = input("Crie uma senha: ")
     
 
         if "@" not in mail:
@@ -40,11 +53,12 @@ class Funcionalidades:
         mail = input("Faça login com seu E-mail!: ")
         senha = input("Senha?: ")
 
-        for Usuario in self.lista_usuarios:
+        for usuario in self.lista_usuarios:
             if (usuario.mail == mail) and (usuario.senha == senha):
 
                 self.usuario_logado = usuario 
                 print(f"Usuario Logado: {self.usuario_logado.nome}")
+                self.menu_funcionalidades()
                 return
 
             print("Usuario inexistente ou Senha inváliida")
@@ -56,7 +70,7 @@ class Funcionalidades:
             print("1. Cadastrar novo usuario")
             print("2. Fazer login")
             print("0. Sair")
-            opcao_escolhida == input("Escolha: ")
+            opcao_escolhida = input("Escolha: ")
 
             if opcao_escolhida == "1":
                 self.cadastrar_usuario()
@@ -70,44 +84,38 @@ class Funcionalidades:
             else:
                 print("Opção inválida!")
             
+    def menu_funcionalidades(self):
+        escolha = ""
+        while escolha != "0":
+            print("------ Tarefas-----")
+            print("1 - Criar Tarefa")
+            print("2 - Listar Tarefas")
+            escolha = input("Selecione: ")
 
+            if escolha == "1":
+                descricao = str(input("Descrição da tarefa?: "))
+                prioridade = str(input("Prioridade?: "))
+                titulo = str(input("titulo?: "))
+                self.usuario_logado.criar_tarefa(descricao,prioridade,titulo)
 
-class SistemasDeTarefas(Tarefa):                        #Herança de Tarefas para Sistemas de tarefas
-    def criar_tarefa(self,descriçao,prioridade,titulo):
-        pass
-
-    
-    def marcar_concluida(self):
-        pass
-
-
-
-
-    def listar_pendentes(self,status):
-        print("/n====Suas tarefas pendentes====")
-        
-                                                                            #Metodos Da classe Sistema de tarefas
-
-    def listar_concluidas(self,status):
-        print("/n====Suas tarefas concluidas")
+            elif escolha == "2":
+                self.usuario_logado.lista_tarefas
 
 
 
-    def listar_tarefas(self): 
+    def listar_tarefas(self,status):                        #Exibir Tarefas
         print("/n====Suas tarefas====")
+        for Tarefa in self.lista_tarefas:
+            print(Tarefa)
+
         
 
-    
-    
-    def remover_tarefa(self):
-        pass
+            
+
 
 
 teste = Funcionalidades()
 teste.menu_principal()
-
-        
-
 
 
 
