30c24288723d2b49c34cf609156c610d01a9eb46
Ildo <ildomoura90@gmail.com>
2025-05-22 11:15:10 -0300

    Projeto finalizado

--- Projeto2.0.py
+++ Projeto2.0.py
@@ -1,14 +1,34 @@
 from abc import ABC, abstractmethod
 
-class Tarefa():                                      #Criação das Classes 
+
+class Tarefa:                                      #Criação das Classes 
+
+    incrementa_id = 1                                   #Criando ID para as tarefas
     def __init__(self,descricao,prioridade,titulo):
+
         self.descriao = descricao
         self.prioridade = prioridade                    #Iniciação dos parâmetros
-        self.titulo = titulo     
+        self.titulo = titulo    
+        self.status = "Pendente" 
+        self.id = Tarefa.incrementa_id
+        Tarefa.incrementa_id += 1
 
     def  __str__(self):
         return f"Tarefa: {self.titulo} - {self.descricao} - {self.prioridade}"                                   
                                  
+
+    def concluir_tarefa(sefl):                      #Concluir Tarefas
+        self.status = "Concluida"
+        print(f"Tarefa {self.titulo} Concluida")
+
+
+    def editar_tarefa(self,novo_titulo,nova_descricao,nova_prioridade):        #Editar tarefas
+        novo_titulo = novo_titulo
+        nova_descricao = nova_descricao
+        nova_prioridade = nova_prioridade
+
+   
+
 class Usuario():
     def __init__(self, nome,mail,senha):
         self.nome = nome
@@ -18,12 +38,24 @@ class Usuario():
 
 
     def criar_tarefa(self,descricao,prioridade,titulo):       # Método para criar tarefas
-        nova_tarefa = Tarefa(titulo,descricao,prioridade)
+        nova_tarefa = Tarefa(descricao, prioridade, titulo)
         self.lista_tarefas.append(nova_tarefa)
 
 
-    def remover_tarefa(self):                   # Método para remover tarefas
-        self.lista_tarefas.remove()
+    def remover_tarefa(self,id_Tarefa):                   # Método para remover tarefas
+        tarefa_buscada = self.proucura_tarefa(id_Tarefa)
+        if tarefa_buscada:
+            self.lista_tarefas.remove(tarefa_buscada)
+
+        else:
+             print("Tarefa não encontrada!")
+
+    def limpar_concluidas(self):
+        tarefas_ativas = []
+        for tarefa in lista_tarefas:
+            if tarefas.status != "Concluida":
+                tarefas_ativas.append(tarefa)
+            self.lista_tarefas = tarefas_ativas
 
 
 
@@ -32,7 +64,7 @@ class Funcionalidades:
         self.lista_usuarios = []
         self.usuario_logado = None
 
-    def cadastrar_usuario(self):  #Cadastrando novo usuario
+    def cadastrar_usuario(self):                     #Cadastrando novo usuario
         nome = input("Digite um nome: ")
         mail = input("Digite o E-mail: ")
         senha = input("Crie uma senha: ")
@@ -47,7 +79,7 @@ class Funcionalidades:
         else:
             novo_usuario = Usuario(nome,mail,senha)
             self.lista_usuarios.append(novo_usuario)
-            print("Usuario cadastrao com sucesso!")
+            print("Usuario cadastrado com sucesso!")
 
     def login(self):                                     #Login de Usuario
         mail = input("Faça login com seu E-mail!: ")
@@ -63,7 +95,7 @@ class Funcionalidades:
 
             print("Usuario inexistente ou Senha inváliida")
 
-    def menu_principal(self):                    #Metodo menu pricipal
+    def menu_principal(self):                    #Menu pricipal
         opcao_escolhida = ""
         while opcao_escolhida != "0":
             print("Bem vindo! Escolha uma opção")
@@ -84,37 +116,91 @@ class Funcionalidades:
             else:
                 print("Opção inválida!")
             
-    def menu_funcionalidades(self):
+    def menu_funcionalidades(self):                         #Menu de funcionalidades
         escolha = ""
         while escolha != "0":
             print("------ Tarefas-----")
             print("1 - Criar Tarefa")
             print("2 - Listar Tarefas")
+            print("3 - Listar Tarefas pendentes")
+            print("4 - Listar Tarefas concluidas")
+            print("5 - Concluir Tarefa")
+            print("6 - Editar Tarefa")
+            print("7 - Remover Tarefa")
+            print("8 - Limpar Concluidas")
+
+
+
             escolha = input("Selecione: ")
 
             if escolha == "1":
                 descricao = str(input("Descrição da tarefa?: "))
-                prioridade = str(input("Prioridade?: "))
+                prioridade = str(input("Prioridade?(Alta,Média ou Baixa): "))
                 titulo = str(input("titulo?: "))
                 self.usuario_logado.criar_tarefa(descricao,prioridade,titulo)
 
             elif escolha == "2":
                 self.usuario_logado.lista_tarefas
 
+            elif escolha == "3":
+                self.usuario_logado.lista_tarefas("Pendente")
+
+            elif escolha == "4":
+                self.usuario_logado.lista_tarefas("Concluida")
+
+            elif escolha == "5":
+                id = input("Insira o ID da Tarefa: ")
+                if id.isdigit():
+                    tarefa_buscada = self.usuario_logado.proucura_tarefa(int(id))
+
+                if tarefa_buscada:
+                    self.usuario_logado.concluir_tarefa()
+
+                else:
+                    print("Tarefa não encontrada!")
 
 
-    def listar_tarefas(self,status):                        #Exibir Tarefas
+            elif escolha == "6":
+                id = input("Insira o ID da Tarefa: ")
+                if id.isdigit():
+                    tarefa_buscada = self.usuario_logado.proucura_tarefa(int(id))
+                    if tarefa_buscada:
+                        novo_titulo = input("Novo Titulo?: ")
+                        nova_descricao = input("Nova descrição?: ")
+                        nova_prioridade = input("Nova prioridade?: ")
+                        tarefa.editar_tarefa(novo_titulo,nova_descricao,nova_prioridade)
+                        
+                    else:
+                        print("Tarefa não encontrada!")
+
+            elif escolha == "7":
+                id = input("Insira o ID da Tarefa: ")
+                if id.isdigit():
+                    tarefa_buscada = self.usuario_logado.remover_tarefa(int(id))
+
+            elif escolha == "8":
+                self.usuario_logado.limpar_concluidas()
+
+
+
+
+
+    def listar_tarefas(self,status=None):                        #Exibir Tarefas
         print("/n====Suas tarefas====")
         for Tarefa in self.lista_tarefas:
-            print(Tarefa)
-
-        
-
-            
+            if status is None or Tarefa.status == status:
+                print(Tarefa)
 
 
+    def proucura_tarefa(self,id_tarefa):
+        for Tarefa in self.lista_tarefas:
+            if Tarefa.id == id_Tarefa:
+                return Tarefa
+        return None
 
-teste = Funcionalidades()
+''
+
+teste = Funcionalidades()               #Exibindo Menu
 teste.menu_principal()
 
 
